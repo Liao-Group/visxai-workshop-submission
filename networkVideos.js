772329a4e75976d37a3ce15d30f9f14c07948bec
shiwen59 <135702207+shiwen59@users.noreply.github.com>
2024-08-06 09:07:57 +0800

    text and network updates

--- networkVideos.js
+++ networkVideos.js
@@ -3,6 +3,19 @@ let selectedNetworkVideoId = {
   'network-section-2': 'newNetworkVideo1'
 };
 
+const networkDescriptions = {
+  'networkVideo1': 'Step 1: The network uses two inputs for each exon - the 70-nucleotide sequence and a predicted secondary structure. ',
+  'networkVideo2': 'Step 2: The network learned a set of features related to exon inclusion and skipping, reflecting both exon sequence and structure. The contribution of each feature is reported as strength arbitrary units, which can vary based upon position along the exon.',
+  'networkVideo3': 'Step 3: The network next calculates the sum of inclusion strengths and the sum of skipping strengths.',
+  'networkVideo4': 'Step 4: The network calculates the difference between inclusion class strength and skipping class strength.',
+  'networkVideo5': 'Step 5: The difference between inclusion and exclusion strengths is then converted into an output prediction using a learned non-linear function.',
+  'newNetworkVideo1': "Step 1: The network considers the input exon's sequence and structure.",
+  'newNetworkVideo2': "Step 2: The contribution strength of each activated inclusion or skipping feature is plotted along the length of the exon. ",
+  'newNetworkVideo3': 'Step 3: The network next calculates the sum of inclusion and skipping strengths.',
+  'newNetworkVideo4': 'Step 4: The network calculates the difference between inclusion class strength and skipping class strength. In this exon, the inclusion strength is greater than skipping strength, leading to a positive delta strength.',
+  'newNetworkVideo5': 'Step 5: The delta strength is converted into an output prediction using a learned non-linear function.'
+};
+
 function selectNetworkVideo(sectionId, videoId, buttonIndex) {
   const section = document.querySelector(`#${sectionId}`);
   const videos = section.querySelectorAll('video');
@@ -28,6 +41,11 @@ function selectNetworkVideo(sectionId, videoId, buttonIndex) {
   const buttonLeft = buttons[buttonIndex].offsetLeft;
   const leftOffset = buttonLeft + (buttonWidth / 2) - (selector.offsetWidth / 2);
   selector.style.left = `${leftOffset}px`;
+
+  // Update the description text
+  const descriptionElementId = `network-description-${sectionId === 'network-section-1' ? '1' : '2'}`;
+  const descriptionElement = section.querySelector(`#${descriptionElementId}`);
+  descriptionElement.textContent = networkDescriptions[videoId];
 }
 
 document.addEventListener('DOMContentLoaded', () => {
